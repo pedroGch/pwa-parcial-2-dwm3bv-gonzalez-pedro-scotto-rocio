9407d7b9e42bcdc80790ab2c25856797dbb6ab03
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-06-28 22:47:42 -0300

    agrego validacion solo del lado del back

--- form_process.php
+++ form_process.php
@@ -5,10 +5,25 @@ $inputApellido = $_POST['inputApellido'];
 $inputEmail = $_POST['inputEmail'];
 $inputComentario = $_POST['inputComentario'];
 
+function soloLetras($str){
+    return preg_match('/^([^0-9]*)$/', $str) and preg_match('/^[a-zA-Z0-9]{4,10}$/', $str);
+}
+
+if (!(soloLetras($inputNombre) and soloLetras($inputApellido))){
+    
+    echo("<div>
+            <h2>Ooops</h2>
+            <p>Los campos nombre y apellido no pueden contener números o caracteres especiales.</p>
+        </div>");
+
+}
+
 if ((isset($inputNombre) and !empty(trim($inputNombre))) and
     (isset($inputApellido) and !empty(trim($inputApellido))) and
     (isset($inputEmail) and !empty(trim($inputEmail))) and
-    (isset($inputComentario) and !empty(trim($inputComentario)))
+    (isset($inputComentario) and !empty(trim($inputComentario))) and 
+    (soloLetras($inputNombre) and soloLetras($inputApellido))
+
 ) {
     echo "<pre>";
     print_r("<div>
@@ -25,18 +40,4 @@ if ((isset($inputNombre) and !empty(trim($inputNombre))) and
          </div>");
     echo "</pre>";
     
-} else {
-    echo "<pre>";
-    print_r("<div>
-                <h2>Ooops</h2>
-                <p>Chequeá los campos ya que alguno/s contienen errores.</p>
-            </div>");
-    echo "</pre>";
 }
-
-
-
-
-
-
-//lo que elijo imprimir en pantalla para mostrar al usuario
